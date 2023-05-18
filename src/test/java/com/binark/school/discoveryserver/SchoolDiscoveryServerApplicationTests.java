f8259b767d30064b43b03bc8e3185a91e080237c
kenany92 <captainnyobe@yahoo.fr>
2023-05-18 13:10:00 +0200

    first commit

---
+++ src/test/java/com/binark/school/discoveryserver/SchoolDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binark.school.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
